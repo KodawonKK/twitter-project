1d4b07540e78babd1d38a89baedd5e0970738d7e
KodawonKK <rhekdnjs0169@gmail.com>
2024-10-21 22:50:46 +0900

    firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCe3LH9iqSr9DimRKWv7U_PuBQPw4QHBss",
+  authDomain: "twitterproject-a7594.firebaseapp.com",
+  projectId: "twitterproject-a7594",
+  storageBucket: "twitterproject-a7594.appspot.com",
+  messagingSenderId: "438884034500",
+  appId: "1:438884034500:web:638266f4283a5bbdfea691",
+  measurementId: "G-1ML43LJP9J"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
