c7bf19e646884ac8ecbcf76fdea85cc89d61ff2a
KodawonKK <rhekdnjs0169@gmail.com>
2024-10-28 22:55:22 +0900

    fix : firebase 계정

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div``;
 const Form = styled.form``;
@@ -7,6 +10,7 @@ const Input = styled.input``;
 const Title = styled.h1``;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -24,10 +28,19 @@ export default function CreateAccount() {
       setEmail(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name
+      });
+      navigate("/");
     } catch (e) {
+      //setError
     } finally {
       setLoading(false);
     }
